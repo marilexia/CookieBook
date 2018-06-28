82f22c87bce06981d6bbe028a2a43014b81b8b28
Marilexia Faith S Acelajado <faithacelajado@gmail.com>
2018-06-28 01:29:00 +0000

    first annotated version

--- models/questions.rb
+++ models/questions.rb
@@ -1,9 +1,12 @@
 def cookie_chooser(user_value)
  if user_value <4
-    "cranberry"
+    "cranberry" 
  elsif user_value <7
     "trailmix"
  elsif user_value <10
     "chocolatey"
+    
+    
+    # cranberry trailmix and chocolatey will be called lateron in the app controller as pathways to respective cookie pages!
 end
 end
